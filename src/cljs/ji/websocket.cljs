54eb9d31aa970b20938af2913668992c8566b843
loganlinn <logan.linn@gmail.com>
2013-10-14 13:30:21 -0500

    Avoid nested go blocks

--- src/cljs/ji/websocket.cljs
+++ src/cljs/ji/websocket.cljs
@@ -11,33 +11,32 @@
   :in   Channel to write values to socket on
   :out  Channel to recieve socket data on"
   ([uri] (connect! uri {}))
-  ([uri {:keys [in out]
-         :or {in chan out chan}}]
+  ([uri {:keys [in out] :or {in chan out chan}}]
    (let [on-connect (chan)]
-     (go
-       (let [in (in)
-             out (out)
-             ws (js/WebSocket. uri)]
-         (doto ws
-           (aset "onopen"
-                 (fn []
-                   (close! on-connect)
-                   (go (loop []
-                         (let [data (<! in)]
-                           (if-not (nil? data)
-                             (do (.send ws (pr-str data))
-                                 (recur))
-                             (do (close! out)
-                                 (.close ws))))))))
-           (aset "onmessage"
-                 (fn [m]
-                   (when-let [data (read-string (.-data m))]
-                     (put! out data))))
-           (aset "onclose"
-                 (fn []
-                   (close! in)
-                   (close! out))))
+     (let [in (in)
+           out (out)
+           ws (js/WebSocket. uri)]
+       (doto ws
+         (aset "onopen"
+               (fn []
+                 (close! on-connect)
+                 (go (loop []
+                       (let [data (<! in)]
+                         (if-not (nil? data)
+                           (do (.send ws (pr-str data))
+                               (recur))
+                           (do (close! out)
+                               (.close ws))))))))
+         (aset "onmessage"
+               (fn [m]
+                 (when-let [data (read-string (.-data m))]
+                   (put! out data))))
+         (aset "onclose"
+               (fn []
+                 (close! in)
+                 (close! out))))
 
+       (go
          (<! on-connect)
          ;; swap in/out for more intuitive naming for receiver
          {:uri uri :ws ws :out in :in out})))))
